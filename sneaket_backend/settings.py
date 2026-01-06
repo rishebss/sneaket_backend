62d57c3db1cecfb5cdca3e63f1b5fadf44242292
Risheb.s <rishebs123456@gmail.com>
2026-01-07 01:59:23 +0530

    added production allowed entry points

--- sneaket_backend/settings.py
+++ sneaket_backend/settings.py
@@ -68,6 +68,7 @@ LOGOUT_REDIRECT_URL = '/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
+    "https://sneaket.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
