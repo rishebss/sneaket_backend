b81920cd7ddf11054fa804808db872df17db87bb
Risheb.s <rishebs123456@gmail.com>
2026-01-07 06:21:52 +0530

    fixed products serializers

--- products/serializers.py
+++ products/serializers.py
@@ -2,20 +2,21 @@ from rest_framework import serializers
 from .models import Sneaker
 
 class SneakerSerializer(serializers.ModelSerializer):
-    img1_url = serializers.SerializerMethodField()
-    img2_url = serializers.SerializerMethodField()
-    img3_url = serializers.SerializerMethodField()
+    # Override the image fields to return URLs directly
+    img1 = serializers.SerializerMethodField()
+    img2 = serializers.SerializerMethodField()
+    img3 = serializers.SerializerMethodField()
     
     class Meta:
         model = Sneaker
-        fields = '__all__'  # Even simpler!
+        fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
     
-    def get_img1_url(self, obj):
+    def get_img1(self, obj):
         return obj.img1.url if obj.img1 else None
     
-    def get_img2_url(self, obj):
+    def get_img2(self, obj):
         return obj.img2.url if obj.img2 else None
     
-    def get_img3_url(self, obj):
+    def get_img3(self, obj):
         return obj.img3.url if obj.img3 else None
