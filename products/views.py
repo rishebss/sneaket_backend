3d6340b15cd61700b62ba63b9381cc6604609fab
Risheb.s <rishebs123456@gmail.com>
2026-02-01 02:51:11 +0530

    added pagination

--- products/views.py
+++ products/views.py
@@ -1,12 +1,20 @@
 # products/views.py
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
 from .models import Sneaker
 from .serializers import SneakerSerializer
 
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class SneakerViewSet(viewsets.ModelViewSet):
     """
     Basic CRUD operations for Sneakers.
-    
+
     ModelViewSet automatically provides:
     - GET /api/sneakers/          -> List all sneakers
     - POST /api/sneakers/         -> Create new sneaker
@@ -15,5 +23,7 @@ class SneakerViewSet(viewsets.ModelViewSet):
     - PATCH /api/sneakers/{id}/   -> Update partial sneaker
     - DELETE /api/sneakers/{id}/  -> Delete sneaker
     """
+
     queryset = Sneaker.objects.all()
+    pagination_class = CustomPagination
     serializer_class = SneakerSerializer
