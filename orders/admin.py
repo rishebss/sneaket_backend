b4354a584f081aa9f13690fb773bc97b3588b8c9
Risheb.s <rishebs123456@gmail.com>
2026-08-17 18:29:08 +0530

    admin update

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, PendingCheckout
 
 
 class OrderItemInline(admin.TabularInline):
@@ -44,3 +44,42 @@ class OrderAdmin(admin.ModelAdmin):
     )
     inlines = [OrderItemInline]
     ordering = ("-created_at",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "sneaker_name",
+        "size",
+        "quantity",
+        "unit_price",
+        "line_total",
+    )
+    list_filter = ("sneaker",)
+    search_fields = ("sneaker_name", "order__order_number")
+    readonly_fields = (
+        "order",
+        "sneaker",
+        "sneaker_name",
+        "sneaker_image",
+        "size",
+        "quantity",
+        "unit_price",
+        "line_total",
+    )
+
+
+@admin.register(PendingCheckout)
+class PendingCheckoutAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "razorpay_order_id",
+        "amount",
+        "created_at",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("razorpay_order_id", "user__username")
+    readonly_fields = ("user", "razorpay_order_id", "amount", "snapshot")
